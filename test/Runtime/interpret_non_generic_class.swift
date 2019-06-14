5f9d1b82d016aa13581e16c5854a43d726fd6ce0
Jiri Dutkevic <jiri@dutkevic.com>
2019-06-13 21:51:51 -0700

    Updated test case.

--- test/Runtime/interpret_non_generic_class.swift
+++ test/Runtime/interpret_non_generic_class.swift
@@ -3,13 +3,39 @@
 
 import Foundation
 
+@_optimize(none) func blackHole<T>(_: T) {}
+
+class C0<T> {
+    
+}
+
+// fixed
 class C1 {
-    var v1: URL?
-    var v2: URL?
-    var v3: URL?
     var v4: String?
     var v5: Int?
     var v6: String?
 }
 
-let _ = C1()
+// singleton
+class C2: C0<Int> {
+    var v1 : URL?
+    var v2 : URL?
+    var v3 : URL?
+    var v4: String?
+    var v5: Int?
+    var v6: String?
+}
+
+// want singleton, had fixedorupdate
+class C3 {
+    var v1 : URL?
+    var v2 : URL?
+    var v3 : URL?
+    var v4: String?
+    var v5: Int?
+    var v6: String?
+}
+
+blackHole(C1())
+blackHole(C2())
+blackHole(C3())
